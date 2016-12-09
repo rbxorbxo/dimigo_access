eb863258b0656a6f7deb913df19f9118ac43464c
ky100499 <leekwangyeon@igwang-yeon-ui-MacBook-Pro.local>
2016-12-09 15:36:09 +0900

    Theme Setting
    Theme
    Main controller

---
+++ application/controllers/Main.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Main extends CI_Controller {
+  public function __construct() {
+    parent::__construct();
+    $this->output->delete_cache();
+  }
+
+	public function index() {
+		$this->load->view('core/head');
+    $this->load->view('core/footer');
+	}
+}
