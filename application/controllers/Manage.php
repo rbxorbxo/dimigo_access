024e32881936e4d19f18d89def7e57e5f95ae506
rbxorbxo <c476262@gmail.com>
2016-12-10 02:16:25 +0900

    에러 해결함
    에러 해결

--- application/controllers/Manage.php
+++ application/controllers/Manage.php
@@ -5,12 +5,17 @@ class Manage extends CI_Controller {
   public function __construct() {
     parent::__construct();
     $this->output->delete_cache();
+    $this->load->model('Manager_model');
   }
 
   public function index() {
+
+    $total = $this->Manager_model->get_request()->result();
+
+
     $this->load->view('core/head', array('title'=>SITE_NAME." - Manage"));
     $this->load->view('core/navbar');
-    $this->load->view('manage/main');
+    $this->load->view('manage/main',$total);
     $this->load->view('core/footer', array("active"=>"manage"));
   }
 }
